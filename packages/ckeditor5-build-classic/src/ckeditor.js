b8c0ac91a59795b0c584a56e788ada9772717df3
Syed Ahsan Ali <syedahsanali995@gmail.com>
2021-12-23 00:14:19 +0500

    new plugin added

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -26,14 +26,14 @@ import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Font from '@ckeditor/ckeditor5-font/src/font';
 import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -66,7 +66,7 @@ ClassicEditor.builtinPlugins = [
 	Alignment,
 	Highlight,
 	Font,
-	HtmlEmbed
+	HtmlEmbed,
 ];
 
 // Editor configuration.
@@ -80,6 +80,7 @@ ClassicEditor.defaultConfig = {
 			'italic',
 			'link',
 			'htmlEmbed',
+			'insertTable',
 			'fontSize', 'fontFamily', 'fontColor','fontBackgroundColor',
 			'alignment',
 			'highlight',
@@ -92,7 +93,10 @@ ClassicEditor.defaultConfig = {
 			'blockQuote',
 			'undo',
 			'redo'
-		]
+		],
+		table: {
+			contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+		}
 	},
 
 	// This value must be kept in sync with the language defined in webpack.config.js.
